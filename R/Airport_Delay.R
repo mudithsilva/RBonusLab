bf00800f2252b5f888c80bb43d74ab13d5bf4647
Mudith Chathuranga <mudithchathuranga@gmail.com>
2019-10-26 13:31:54 +0200

    Added Travis Added Vignetts Added Airport Delay Function

---
+++ R/Airport_Delay.R
@@ -0,0 +1,25 @@
+#' Airport Delay Visualise on a ggplot
+#'
+#' @return ggplot about airport delays
+#' @import nycflights13
+#' @export visualize_airport_delays
+#'
+#' @examples
+#' visualize_airport_delays()
+
+
+visualize_airport_delays <- function() {
+
+  flights <- nycflights13::flights
+  airports <- nycflights13::airports
+
+  mean_data <- dplyr::summarise(dplyr::group_by(flights, dest), M = mean(arr_delay))
+
+  combine_data <- dplyr::inner_join(airports,mean_data, by = c("faa" =  "dest"))
+
+  plot_data <- ggplot2::ggplot(combine_data, aes(x = combine_data$lat, y = combine_data$lon)) +
+    geom_point()
+
+  return(plot_data)
+
+}
